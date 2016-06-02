dd03244283ef25d4335524ed193d9c64bd3226f6
developer-account <irteam@dev-lynix.ncl>
2016-06-02 20:54:35 +0900

    resolve conflict of tuple without glr-parser

--- src/parser/parser.hpp
+++ src/parser/parser.hpp
@@ -1,19 +1,19 @@
-/* A Bison parser, made by GNU Bison 3.0.2.  */
+/* A Bison parser, made by GNU Bison 2.7.  */
 
-/* Skeleton interface for Bison GLR parsers in C
-
-   Copyright (C) 2002-2013 Free Software Foundation, Inc.
-
+/* Bison interface for Yacc-like parsers in C
+   
+      Copyright (C) 1984, 1989-1990, 2000-2012 Free Software Foundation, Inc.
+   
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
    the Free Software Foundation, either version 3 of the License, or
    (at your option) any later version.
-
+   
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
    GNU General Public License for more details.
-
+   
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
@@ -26,13 +26,13 @@
    special exception, which will cause the skeleton and the resulting
    Bison output files to be licensed under the GNU General Public
    License without this special exception.
-
+   
    This special exception was added by the Free Software Foundation in
    version 2.2 of Bison.  */
 
 #ifndef YY_YY_SRC_PARSER_PARSER_HPP_INCLUDED
 # define YY_YY_SRC_PARSER_PARSER_HPP_INCLUDED
-/* Debug traces.  */
+/* Enabling traces.  */
 #ifndef YYDEBUG
 # define YYDEBUG 0
 #endif
@@ -40,90 +40,91 @@
 extern int yydebug;
 #endif
 
-/* Token type.  */
+/* Tokens.  */
 #ifndef YYTOKENTYPE
 # define YYTOKENTYPE
-  enum yytokentype
-  {
-    NEW_LINE = 258,
-    STATIC = 259,
-    DEF = 260,
-    CLONE = 261,
-    MY = 262,
-    OWNER = 263,
-    CALLER = 264,
-    ROOT = 265,
-    MODULE = 266,
-    PARENT = 267,
-    IF = 268,
-    ELIF = 269,
-    ELSE = 270,
-    DO = 271,
-    WHILE = 272,
-    TIMES = 273,
-    SWITCH = 274,
-    DECODE = 275,
-    PARSE = 276,
-    FOR = 277,
-    IN = 278,
-    RETURN_ = 279,
-    CONTINUE = 280,
-    BREAK = 281,
-    EQ = 282,
-    NEQ = 283,
-    AND = 284,
-    OR = 285,
-    GT = 286,
-    GE = 287,
-    LT = 288,
-    LE = 289,
-    TRY = 290,
-    CATCH = 291,
-    FINALLY = 292,
-    THROW = 293,
-    DUMP = 294,
-    STACK = 295,
-    LOCAL = 296,
-    USING = 297,
-    TRUE = 298,
-    FALSE = 299,
-    DOUBLE_DOT = 300,
-    TRIPLE_DOT = 301,
-    ADD_ASSIGN = 302,
-    SUB_ASSIGN = 303,
-    MUL_ASSIGN = 304,
-    DIV_ASSIGN = 305,
-    PURE = 306,
-    NIL_ = 307,
-    RIGHT_ARROW = 308,
-    LEFT_ARROW = 309,
-    PARALLEL = 310,
-    ASSIGNMENT = 311,
-    ONCE = 312,
-    NAME = 313,
-    STRING = 314,
-    FORMAT_STRING_HEAD = 315,
-    FORMAT_STRING_TAIL = 316,
-    RE_STRING = 317,
-    RE_FORMAT_STRING_HEAD = 318,
-    HEX_STRING = 319,
-    SHELL_STRING_HEAD = 320,
-    CONTEXT = 321,
-    NUMBER = 322,
-    MINUS_NUMBER = 323,
-    BIG_NUMBER = 324,
-    MINUS_BIG_NUMBER = 325,
-    REAL = 326,
-    MINUS_REAL = 327
-  };
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     NEW_LINE = 258,
+     STATIC = 259,
+     DEF = 260,
+     CLONE = 261,
+     MY = 262,
+     OWNER = 263,
+     CALLER = 264,
+     ROOT = 265,
+     MODULE = 266,
+     PARENT = 267,
+     IF = 268,
+     ELIF = 269,
+     ELSE = 270,
+     DO = 271,
+     WHILE = 272,
+     TIMES = 273,
+     SWITCH = 274,
+     DECODE = 275,
+     PARSE = 276,
+     FOR = 277,
+     IN = 278,
+     RETURN_ = 279,
+     CONTINUE = 280,
+     BREAK = 281,
+     EQ = 282,
+     NEQ = 283,
+     AND = 284,
+     OR = 285,
+     GT = 286,
+     GE = 287,
+     LT = 288,
+     LE = 289,
+     TRY = 290,
+     CATCH = 291,
+     FINALLY = 292,
+     THROW = 293,
+     DUMP = 294,
+     STACK = 295,
+     LOCAL = 296,
+     USING = 297,
+     TRUE = 298,
+     FALSE = 299,
+     DOUBLE_DOT = 300,
+     TRIPLE_DOT = 301,
+     ADD_ASSIGN = 302,
+     SUB_ASSIGN = 303,
+     MUL_ASSIGN = 304,
+     DIV_ASSIGN = 305,
+     PURE = 306,
+     NIL_ = 307,
+     RIGHT_ARROW = 308,
+     LEFT_ARROW = 309,
+     PARALLEL = 310,
+     ASSIGNMENT = 311,
+     ONCE = 312,
+     NAME = 313,
+     STRING = 314,
+     FORMAT_STRING_HEAD = 315,
+     FORMAT_STRING_TAIL = 316,
+     RE_STRING = 317,
+     RE_FORMAT_STRING_HEAD = 318,
+     HEX_STRING = 319,
+     SHELL_STRING_HEAD = 320,
+     CONTEXT = 321,
+     NUMBER = 322,
+     MINUS_NUMBER = 323,
+     BIG_NUMBER = 324,
+     MINUS_BIG_NUMBER = 325,
+     REAL = 326,
+     MINUS_REAL = 327
+   };
 #endif
 
-/* Value type.  */
+
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
-typedef union YYSTYPE YYSTYPE;
-union YYSTYPE
+typedef union YYSTYPE
 {
-#line 49 "../src/parser/parser.y" /* glr.c:2555  */
+/* Line 2058 of yacc.c  */
+#line 49 "../src/parser/parser.y"
 
 	const char* cp;
 	int integer;
@@ -133,15 +134,29 @@ union YYSTYPE
 	struct cp2_t cp2; 
 	struct int2_t int2;
 
-#line 137 "../src/parser/parser.hpp" /* glr.c:2555  */
-};
+
+/* Line 2058 of yacc.c  */
+#line 140 "../src/parser/parser.hpp"
+} YYSTYPE;
 # define YYSTYPE_IS_TRIVIAL 1
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
 # define YYSTYPE_IS_DECLARED 1
 #endif
 
-
 extern YYSTYPE yylval;
 
+#ifdef YYPARSE_PARAM
+#if defined __STDC__ || defined __cplusplus
+int yyparse (void *YYPARSE_PARAM);
+#else
+int yyparse ();
+#endif
+#else /* ! YYPARSE_PARAM */
+#if defined __STDC__ || defined __cplusplus
 int yyparse (void);
+#else
+int yyparse ();
+#endif
+#endif /* ! YYPARSE_PARAM */
 
 #endif /* !YY_YY_SRC_PARSER_PARSER_HPP_INCLUDED  */
