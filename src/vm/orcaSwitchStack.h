de4f393eea74ff8cc0745612f78a71cd317c4081
Lee, Ki-Yeul <lynix94@gmail.com>
2017-12-31 01:28:49 +0900

    add omitted file of prev commit

---
+++ src/vm/orcaSwitchStack.h
@@ -0,0 +1,65 @@
+/**********************************************************************
+
+  orcaSwitchStack.h - class for handling "switch statement"
+
+  Copyright (C) 2009- Lee, Ki-Yeul
+
+**********************************************************************/
+
+#ifndef _DEF_ORCA_SWITCH_STACK
+#define _DEF_ORCA_SWITCH_STACK
+
+class orcaList;
+class orcaMap;
+class orcaVM;
+
+#include "orcaListIter.h"
+#include "orcaMapIter.h"
+
+class orcaSwitchStack
+{
+public:
+	orcaSwitchStack(orcaVM* vm) : m_vm(vm) {};
+
+	struct SWITCH 
+	{
+		orcaData lhs;
+	};
+
+	bool push(orcaData& d)
+	{
+		SWITCH sw;
+		sw.lhs = d;
+		m_stack.push_back(sw);
+	};
+
+	SWITCH* top()
+	{
+		if (m_stack.empty()) {
+			return NULL;
+		}
+
+		SWITCH *sw = &m_stack[m_stack.size()-1];
+		return sw;
+	};
+
+	void pop()
+	{
+		m_stack.pop_back();
+	};
+
+	bool compare(orcaData &rhs)
+	{
+		SWITCH* sw = top();
+		return sw->lhs == rhs;
+	};
+
+private:
+	vector<SWITCH> m_stack;
+	orcaVM* m_vm;
+};
+
+
+#endif
+
+
