465ed68ad0dd39adfd32a2d0b9bb76f3ad7601e3
Lee, Ki-Yeul <kiyeul.lee@gmail.com>
2019-05-04 14:55:06 -0400

    fix thread parameter bug & tests

---
+++ src/corelib/orcaThread.cpp
@@ -0,0 +1,84 @@
+/* vim: set fdc=2 foldmethod=marker ts=4 tabstop=4 sw=4 sts=4 : */
+
+#include "orcaThread.h"
+
+orcaThread::orcaThread() 
+{
+	set_name("thread");
+
+	insert_native_function("run", (object_fp)&orcaThread::ex_run);
+	insert_native_function("join", (object_fp)&orcaThread::ex_join);
+}
+
+orcaData orcaThread::ex_run(orcaVM* vm, int n) 
+{
+	if (n<1) {
+		return NIL;
+	}
+
+	orcaThread* tp = new orcaThread();
+
+	tp->m_arg = vm->get_param(0).Object();
+	tp->m_tid = 0;
+
+	for(int i=1; i<n; i++) {
+		tp->m_param.push_back(vm->get_param(i));
+	}
+
+	if(pthread_create(&tp->m_tid, NULL, orcaThread::entryPoint, tp) < 0) {
+		printf("Thread Start Error, ThreadID: %d\n", (int)tp->m_tid);
+		return NIL;
+	}
+
+	m_remains.insert(tp->m_tid);
+	return tp;
+}
+
+orcaData orcaThread::ex_join(orcaVM* vm, int n) 
+{
+	long long status = 0;
+	pthread_join(m_tid, (void**)&status);
+	m_remains.erase(m_remains.find(m_tid));
+
+	return (int)status;
+}
+
+void orcaThread::execute(orcaObject* arg) {
+	m_vm.init();
+
+	m_vm.m_module = g_root;
+	m_vm.m_curr = g_root;
+
+	m_vm.m_local->increase(1024);
+	m_vm.push_stack(arg);
+	arg->rc_inc();
+
+	for (int i=0; i<m_param.size(); i++) {
+		m_vm.push_stack(m_param[i]);
+	}
+
+	try {
+		m_vm.call(m_param.size());
+	}
+	catch(orcaException& e) {
+		printf("uncaugted exception in thread: %s %s\n", e.who(), e.what());
+		cout << e.m_stack_trace << endl;
+
+		arg->rc_dec();
+		m_vm.cleanup();
+		return;
+	}
+
+	arg->rc_dec();
+	m_vm.cleanup();
+}
+
+void orcaThread::join_remains()
+{
+	long long status = 0;
+	set<pthread_t>::iterator it = m_remains.begin();
+	for (; it != m_remains.end(); ++it) {
+		pthread_join((*it), (void**)&status);
+	}
+}
+
--- src/corelib/orcaThread.h
+++ src/corelib/orcaThread.h
@@ -36,46 +36,9 @@ class LIBORCA_API orcaThread : public orcaObject
 public:
 	DEFAULT_NATIVE_DEFINE(orcaThread);
 
-	orcaThread() 
-	{
-		set_name("thread");
-
-		insert_native_function("run", (object_fp)&orcaThread::ex_run);
-		insert_native_function("join", (object_fp)&orcaThread::ex_join);
-	}
-
-	orcaData ex_run(orcaVM* vm, int n) 
-	{
-		if (n<1) {
-			return NIL;
-		}
-
-		orcaThread* tp = new orcaThread();
-
-		tp->m_arg = vm->get_param(0).Object();
-		tp->m_tid = 0;
-
-		for(int i=1; i<n; i++) {
-			tp->m_param.push_back(vm->get_param(i));
-		}
-
-		if(pthread_create(&tp->m_tid, NULL, orcaThread::entryPoint, tp) < 0) {
-			printf("Thread Start Error, ThreadID: %d\n", (int)tp->m_tid);
-			return NIL;
-		}
-
-		m_remains.insert(tp->m_tid);
-		return tp;
-	}
-
-	orcaData ex_join(orcaVM* vm, int n) 
-	{
-		long long status = 0;
-		pthread_join(m_tid, (void**)&status);
-		m_remains.erase(m_remains.find(m_tid));
-
-		return (int)status;
-	}
+	orcaThread();
+	orcaData ex_run(orcaVM* vm, int n);
+	orcaData ex_join(orcaVM* vm, int n);
 
 public:
 	orcaObject* m_arg;
@@ -86,35 +49,7 @@ public:
 		return 0;
 	}
 
-	virtual void execute(orcaObject* arg) {
-		m_vm.init();
-
-		m_vm.m_module = g_root;
-		m_vm.m_curr = g_root;
-
-		m_vm.m_local->increase(1024);
-		m_vm.push_stack(arg);
-		arg->rc_inc();
-
-		for (int i=0; i<m_param.size(); i++) {
-			m_vm.push_stack(m_param[i]);
-		}
-
-		try {
-			m_vm.call(m_param.size());
-		}
-		catch(orcaException& e) {
-			printf("uncaugted exception in thread: %s %s\n", e.who(), e.what());
-			cout << e.m_stack_trace << endl;
-
-			arg->rc_dec();
-			m_vm.cleanup();
-			return;
-		}
-
-		arg->rc_dec();
-		m_vm.cleanup();
-	}
+	virtual void execute(orcaObject* arg);
 
 private:
 	orcaVM m_vm;
@@ -123,15 +58,7 @@ private:
 	static set<pthread_t> m_remains;
 
 public:
-	static void join_remains()
-	{
-		int status = 0;
-
-		set<pthread_t>::iterator it = m_remains.begin();
-		for (; it != m_remains.end(); ++it) {
-			pthread_join((*it), (void**)&status);
-		}
-	}
+	static void join_remains();
 };
 
 
